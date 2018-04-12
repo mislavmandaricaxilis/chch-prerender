9a6c34a8cd42b6a909f1a1e52690aff4d76894b4
Mislav Mandaric <mislav@Mislavs-MacBook-Pro-2.local>
2018-04-12 10:18:32 +0200

    Added hydration instead of rendering

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-// const root = document.getElementById('root');
-// if (root.hasChildNodes()) {
-//     ReactDOM.hydrate(<App />, root);
-// } else {
-//     ReactDOM.render(<App />, root);
-// }
+// ReactDOM.render(<App />, document.getElementById('root'));
+const root = document.getElementById('root');
+if (root.hasChildNodes()) {
+    ReactDOM.hydrate(<App />, root);
+} else {
+    ReactDOM.render(<App />, root);
+}
